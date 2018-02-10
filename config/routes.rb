c8e33c7521768c2da863b5bf16e9eb5fbfab4da4
ruirosillas <rui21rafael@gmail.com>
2018-02-09 22:35:40 -0800

    added home and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  # set the index page / root url
+  root 'pages#home'
+
+  get 'pages/home'
+
+  get 'pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
