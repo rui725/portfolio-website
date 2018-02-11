8b545eafdcb293a022497dcf64056b31083cc472
ruirosillas <rui21rafael@gmail.com>
2018-02-11 12:21:11 -0800

    changed front-page layout

--- app/assets/javascripts/components/frontpage.js.jsx
+++ app/assets/javascripts/components/frontpage.js.jsx
@@ -8,7 +8,7 @@ class Frontpage extends React.Component{
         <h1>&lt; R &#124; Я &gt;</h1>
         <p>Hi, I'm Rui, I'm a Software Engineer based in Vancouver, British Columbia, Canada,
          who has worked with different Technologies Ranging from Web and Mobile Development, Desktop Application, and Software Testing.</p>
-         <button class="btn btn-primary" onclick="window.location.href='/about'"> Learn More About Me</button>
+         <a href="/about"><button class="btn btn-primary"> Learn More About Me</button></a>
        </div>
        </div>
     )
