d0389918641d93f526e82d0182b5e1bc938c93e7
ruirosillas <rui21rafael@gmail.com>
2018-02-10 21:53:41 -0800

    added front-page

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   # set the index page / root url
   root 'pages#home'
 
-  get 'pages/home'
+  get '/about' => 'pages#about'
+ 
 
-  get 'pages/about'
+  #get 'pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
