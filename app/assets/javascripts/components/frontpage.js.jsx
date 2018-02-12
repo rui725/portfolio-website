7c07170b05c844de1be675c762bd95973f4cc9a8
ruirosillas <rui21rafael@gmail.com>
2018-02-11 21:54:33 -0800

    Added stuff frontpage

--- app/assets/javascripts/components/frontpage.js.jsx
+++ app/assets/javascripts/components/frontpage.js.jsx
@@ -1,17 +1,45 @@
-class Frontpage extends React.Component{
-  render(){
-    return(
-    <div>
-    <div id="bg-page">
-      </div>
-      <div id="front-page">
-        <h1>&lt; R &#124; Я &gt;</h1>
-        <p>Hi, I'm Rui, I'm a Software Engineer based in Vancouver, British Columbia, Canada,
-         who has worked with different Technologies Ranging from Web and Mobile Development, Desktop Application, and Software Testing.</p>
-         <a href="/about"><button class="btn btn-primary"> Learn More About Me</button></a>
-       </div>
-       </div>
-    )
-  }
 
+
+function Logo(){
+  return(
+      <h1>Я&#124;R</h1>
+
+  );
 }
+
+function MyPic(props){
+    return(
+            <img id="mypic" src={props.front_pic } />
+    );
+}
+
+function Paragraph(){
+  return(
+    <p>Hi, I'm Rui, I'm a Software Engineer based in Vancouver, British Columbia, Canada,
+   who has worked with different Technologies Ranging from Web and Mobile Development, Desktop Application, and Software Testing.</p>
+  );
+}
+
+function IndexButton(){
+    return(
+         <a href="/about"><button class="btn btn-primary">Click Here to Learn more</button></a>
+    );
+}
+
+
+window.Frontpage = createReactClass({
+    render: function(){
+        return(
+              <div>
+               <div id="bg-page">
+               </div>
+                 <div id="front-page">
+                  <Logo />
+                  <MyPic front_pic={this.props.front_pic}/>
+                  <Paragraph />
+                  <IndexButton />
+                 </div>
+             </div>
+             )
+    }
+});
