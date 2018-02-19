9adc692c0ab3b33d8e9eddbb8919cf280f3fd781
ruirosillas <rui21rafael@gmail.com>
2018-02-18 22:37:32 -0800

    added summary

--- app/assets/javascripts/components/footer.js.jsx
+++ app/assets/javascripts/components/footer.js.jsx
@@ -11,13 +11,18 @@ window.Footer = createReactClass({
 function FootParagraph(){
     return(
         <div>
+           <p>
+               <h2>Contact</h2>
+                <a href="https://www.linkedin.com/in/ruirosillas/"><img width="20px" src={'https://cdn1.iconfinder.com/data/icons/logotypes/32/square-linkedin-128.png'} alt="LinkedIn"/> Rui Rafael Rosillas</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      <a href="mailto:ruirosillas@gmail.com?Subject=Hi%20PortfolioWebsite"><img width="20px" src={'https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/mail-128.png'} alt="Mail"/> ruirosillas@gmail.com</a>
+
+           </p>
             <p>
                     Powered By <img width="20px" src={'https://thinkster.io/assets/homepage/rails-9b13de39de9d2eed10a2418734ee1e584cdc9634c04206af1c2bfcc8b0d0e3d5.png'} alt="Ruby" />
-                          <img width="40px" src={'https://sites.udel.edu/enesavc/files/2017/10/1-1hh3nl3.png'}/>
-                          <img width="40px" src={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'}/>
-                          <img width="40px" src={'https://softwareengineeringdaily.com/wp-content/uploads/2016/10/herokukafka.png'} />
+                          <img width="40px" src={'https://sites.udel.edu/enesavc/files/2017/10/1-1hh3nl3.png'} alt="GitHub"/>
+                          <img width="40px" src={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'} alt="ReactJS"/>
+                          <img width="40px" src={'https://softwareengineeringdaily.com/wp-content/uploads/2016/10/herokukafka.png'} alt="Heroku" />
           </p>
-          <h6>Created by Rui Rafael Rosillas</h6>
         </div>
     );
 }
--- app/assets/javascripts/components/indexbody.js.jsx
+++ app/assets/javascripts/components/indexbody.js.jsx
@@ -1,9 +1,36 @@
 window.IndexBody = createReactClass({
     render: function(){
         return(
-          <div>
-          TEST
+          <div id="AboutBody">
+            <AboutMe />
+            <Skills />
           </div>
         );
     }
 });
+
+function GridLayout(){
+  return(
+    <div></div>
+  );
+}
+
+function AboutMe(){
+    return(
+        <div id="summary">
+            Summary
+            <p>	&#10077;
+              A strong and passionate individual with a background in Computer Science focusing the field of Software Development and Software Quality Assurance.
+              A hands-on individual tackling in different sets of technologies involving Mobile Android Application, Backend Web Development Application, Desktop Application Development, and Automation Application.
+            &#10080;</p>
+      </div>
+    );
+}
+
+function Skills(){
+    return(
+      <div id="plskills">
+
+      </div>
+    );
+}
