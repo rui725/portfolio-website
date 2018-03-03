0e4257b552228f5d0a77b328253c1466a8e59145
ruirosillas <rui21rafael@gmail.com>
2018-03-02 23:08:16 -0800

    fix multibrowser problem

--- app/assets/javascripts/components/workid.js.jsx
+++ app/assets/javascripts/components/workid.js.jsx
@@ -12,9 +12,11 @@ function Project(p){
     return(
       <div>
          <a href={p.p.info_link}><h2>{p.p.title}</h2></a>
+        <h4>Technology: {p.p.technology}</h4>
+
          <img src={p.p.imgLink} />
          <p>{p.p.description}</p>
-         <h4>Technology: {p.p.technology}</h4>
+
 
       </div>
     );
